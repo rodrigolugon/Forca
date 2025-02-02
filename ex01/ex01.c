b6af4eccd433737437e69b4902e61edb1f5d7eca
rodrigolugon <rodrigolugonn@gmail.com>
2025-02-02 11:47:20 -0300

    Adicionar arquivos de projeto.

---
+++ ex01/ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int notas[10];
+
+	notas[0] = 10;
+	notas[2] = 5;
+	notas[8] = 9;
+	notas[9] = 4;
+
+	printf("%d %d %d", notas[0], notas[8], notas[9]);
+
+	return 0;
+}
